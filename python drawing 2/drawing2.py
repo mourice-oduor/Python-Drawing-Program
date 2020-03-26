9db9e2faae177892594c4ebf674b0091d705dc65
mourice-oduor <otienomaurice364@gmail.com>
2020-03-25 23:34:06 -0400

    drawing app version 2

--- python drawing 2/drawing2.py
+++ python drawing 2/drawing2.py
@@ -2,69 +2,69 @@ from tkinter import *
 from tkinter import ttk, colorchooser
 
 class main:
-    def __init__(self,master):
-        self.master = master
-        self.color_fg = 'black'
-        self.color_bg = 'white'
-        self.old_x = None
-        self.old_y = None
-        self.penwidth = 5
-        self.drawWidgets()
-        self.c.bind('<B1-Motion>',self.paint)#drwaing the line 
-        self.c.bind('<ButtonRelease-1>',self.reset)
+    def __init__(painter,master):
+        painter.master = master
+        painter.color_fg = 'black'
+        painter.color_bg = 'white'
+        painter.old_x = None
+        painter.old_y = None
+        painter.penwidth = 5
+        painter.drawWidgets()
+        painter.c.bind('<B1-Motion>',painter.paint)#drwaing the line 
+        painter.c.bind('<ButtonRelease-1>',painter.reset)
 
-    def paint(self,e):
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x,self.old_y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
+    def paint(painter,e):
+        if painter.old_x and painter.old_y:
+            painter.c.create_line(painter.old_x,painter.old_y,e.x,e.y,width=painter.penwidth,fill=painter.color_fg,capstyle=ROUND,smooth=True)
 
-        self.old_x = e.x
-        self.old_y = e.y
+        painter.old_x = e.x
+        painter.old_y = e.y
 
-    def reset(self,e):    #reseting or cleaning the canvas 
-        self.old_x = None
-        self.old_y = None      
+    def reset(painter,e):    #reseting or cleaning the canvas 
+        painter.old_x = None
+        painter.old_y = None      
 
-    def changeW(self,e): #change Width of pen through slider
-        self.penwidth = e
+    def changeW(painter,e): #change Width of pen through slider
+        painter.penwidth = e
            
 
-    def clear(self):
-        self.c.delete(ALL)
+    def clear(painter):
+        painter.c.delete(ALL)
 
-    def change_fg(self):  #changing the pen color
-        self.color_fg=colorchooser.askcolor(color=self.color_fg)[1]
+    def change_fg(painter):  #changing the pen color
+        painter.color_fg=colorchooser.askcolor(color=painter.color_fg)[1]
 
-    def change_bg(self):  #changing the background color canvas
-        self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
-        self.c['bg'] = self.color_bg
+    def change_bg(painter):  #changing the background color canvas
+        painter.color_bg=colorchooser.askcolor(color=painter.color_bg)[1]
+        painter.c['bg'] = painter.color_bg
 
-    def drawWidgets(self):
-        self.controls = Frame(self.master,padx = 5,pady = 5)
-        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
-        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=VERTICAL)
-        self.slider.set(self.penwidth)
-        self.slider.grid(row=0,column=1,ipadx=30)
-        self.controls.pack(side=LEFT)
+    def drawWidgets(painter):
+        painter.controls = Frame(painter.master,padx = 5,pady = 5)
+        Label(painter.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
+        painter.slider = ttk.Scale(painter.controls,from_= 5, to = 100,command=painter.changeW,orient=VERTICAL)
+        painter.slider.set(painter.penwidth)
+        painter.slider.grid(row=0,column=1,ipadx=30)
+        painter.controls.pack(side=LEFT)
         
-        self.c = Canvas(self.master,width=500,height=400,bg=self.color_bg,)
-        self.c.pack(fill=BOTH,expand=True)
+        painter.c = Canvas(painter.master,width=500,height=400,bg=painter.color_bg,)
+        painter.c.pack(fill=BOTH,expand=True)
 
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
+        menu = Menu(painter.master)
+        painter.master.config(menu=menu)
         filemenu = Menu(menu)
         colormenu = Menu(menu)
         menu.add_cascade(label='Colors',menu=colormenu)
-        colormenu.add_command(label='Brush Color',command=self.change_fg)
-        colormenu.add_command(label='Background Color',command=self.change_bg)
+        colormenu.add_command(label='Brush Color',command=painter.change_fg)
+        colormenu.add_command(label='Background Color',command=painter.change_bg)
         optionmenu = Menu(menu)
         menu.add_cascade(label='Options',menu=optionmenu)
-        optionmenu.add_command(label='Clear Canvas',command=self.clear)
-        optionmenu.add_command(label='Exit',command=self.master.destroy) 
+        optionmenu.add_command(label='Clear Canvas',command=painter.clear)
+        optionmenu.add_command(label='Exit',command=painter.master.destroy) 
         
         
 
 if __name__ == '__main__':
     root = Tk()
     main(root)
-    root.title('Application')
+    root.title('Python Drawing Application')
     root.mainloop()
